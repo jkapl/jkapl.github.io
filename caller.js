fd852dfe040d21ad2fe7f586dcdafbe29a464050
Joel <joelkaplan1@gmail.com>
2020-04-13 10:52:06 -0700

    Try to fix one way video bug

--- caller.js
+++ caller.js
@@ -25,6 +25,11 @@ let receiver = new RTCPeerConnection(server);
 
 // Caller
 async function getVideo () {
+  let sessDescription = await caller.createOffer();
+  console.log(JSON.stringify(sessDescription))
+
+  caller.setLocalDescription(sessDescription)
+
   const stream = await navigator.mediaDevices.getUserMedia( { video: true });
   gotStream(stream);
 }
@@ -41,10 +46,10 @@ async function gotStream (stream) {
   //   caller.setLocalDescription(offer);
   // });
 
-  let sessDescription = await caller.createOffer();
-  console.log(JSON.stringify(sessDescription))
+  // let sessDescription = await caller.createOffer();
+  // console.log(JSON.stringify(sessDescription))
 
-  caller.setLocalDescription(sessDescription)
+  // caller.setLocalDescription(sessDescription)
 
   caller.onicecandidate = e => {
     if (!e.candidate) return
